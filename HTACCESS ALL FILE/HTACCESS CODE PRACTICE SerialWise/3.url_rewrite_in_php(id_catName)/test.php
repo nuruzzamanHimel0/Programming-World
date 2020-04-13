51c3ce3171cebe679fdb609efca35dfa40d2e236
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2020-04-13 10:14:05 +0600

    asdasf

--- HTACCESS ALL FILE/HTACCESS CODE PRACTICE SerialWise/3.url_rewrite_in_php(id_catName)/test.php
+++ HTACCESS ALL FILE/HTACCESS CODE PRACTICE SerialWise/3.url_rewrite_in_php(id_catName)/test.php
@@ -15,6 +15,16 @@
 	 ?>
 		<a href="user/<?php echo $i; ?>/<?php echo "Cat_name".$cat_name;  ?>" target="_blank"> Item <?php echo $i; ?></a>	<br>
 	<?php	}?>
+
+	<br><br>
+
+	<?php 
+	$cat_name = 99;
+	for ($i=1; $i <10 ; $i++) {
+		$cat_name = $cat_name-1;
+	 ?>
+		<a href="admin/user/<?php echo $i; ?>/<?php echo "Cat_name".$cat_name;  ?>" target="_blank"> Admin Item <?php echo $i; ?></a>	<br>
+	<?php	}?>
 </body>
 <a href="" ></a>
 </html>
